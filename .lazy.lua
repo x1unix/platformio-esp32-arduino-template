07fccd749dc2d2e15f5f0fa45f26c348d9636e43
Denys Sedchenko <9203548+x1unix@users.noreply.github.com>
2026-08-23 13:57:00 -0400

    update clangd config for pioarduino

--- .lazy.lua
+++ .lazy.lua
@@ -1,17 +1,17 @@
 local function require_executable(path)
-	assert(vim.fn.executable(path) == 1, "Required executable not found: " .. path .. ". Run 'make compiledb' to install toolchain.")
+	assert(vim.fn.executable(path) == 1, "Required executable not found: " .. path)
 
 	return path
 end
 
--- clang lsp config with correct toolchain path.
 return {
 	{
 		"neovim/nvim-lspconfig",
 		init = function()
 			local clangd = require_executable(vim.fn.stdpath("data") .. "/mason/bin/clangd")
 
-			local toolchain_bin = vim.fn.expand("~/.platformio/packages/toolchain-xtensa-esp32/bin")
+			-- NOTE: This is pioarduino toolchain path.
+			local toolchain_bin = vim.fn.expand("~/.platformio/packages/toolchain-xtensa-esp-elf/bin")
 
 			local gcc = require_executable(toolchain_bin .. "/xtensa-esp32-elf-gcc")
 
